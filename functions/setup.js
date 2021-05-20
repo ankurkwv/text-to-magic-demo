140b71cb743ff316684360f1452d8b0ee0de8be2
Ankur Kumar <me@ankurk.com>
2021-05-20 09:35:37 -0400

    Formatting updates & comments

--- functions/setup.js
+++ functions/setup.js
@@ -1,15 +1,16 @@
-exports.handler = async function (context, event, callback) {
+exports.handler = async function(context, event, callback) {
   var fs = require('fs');
 
+  // Check if we already ran and prevent duplicate setups.
   if (context.SYNC_SERVICE_SID && context.TWILIO_API_KEY && context.TWILIO_API_SECRET) {
-    return callback(null, {
-      syncServiceSid: context.SYNC_SERVICE_SID
-    });
+    return callback(null, 'Previously ran!');
   }
 
+  // Get the dynamic Studio flow base JSON content
   const assets = Runtime.getAssets();
   let flowDefinition = fs.readFileSync(assets["/studio.json"].path, 'utf8');
 
+  // The Twilio node Client library 
   const client = context.getTwilioClient();
 
   // Substitue the correct URL into the studio flow JSON
@@ -22,41 +23,57 @@ exports.handler = async function (context, event, callback) {
   // Deploy Twilio Studio Flow
   function deployStudio() {
     return client.studio.flows
-        .create({
-          friendlyName: 'Text to Magic',
-          status: 'published',
-          definition: flowDefinition,
-        })
-        .then((flow) => flow)
-        .catch((err) => { console.log(err.details); throw new Error(err.details) });
+      .create({
+        friendlyName: 'Text to Magic',
+        status: 'published',
+        definition: flowDefinition,
+      })
+      .then((flow) => flow)
+      .catch((err) => {
+        console.log(err.details);
+        throw new Error(err.details)
+      });
   }
 
+  // Creating API Key for use with Sync later
   function createApiKey() {
     return client.newKeys.create()
-        .then(new_key => new_key)
-        .catch((err) => { throw new Error(err.details) });
+      .then(new_key => new_key)
+      .catch((err) => {
+        throw new Error(err.details)
+      });
   }
 
+  // Sync Service will be the data-store and event broadcaster
   function createSyncService() {
     return client.sync.services.create()
-        .then(service => service)
-        .catch((err) => { throw new Error(err.details) });
+      .then(service => service)
+      .catch((err) => {
+        throw new Error(err.details)
+      });
   }
 
+  // The Sync List will be where specifically our MagicTexters live
   function createSyncList(serviceSid) {
     return client.sync.services(serviceSid)
-        .syncLists
-        .create({
-          uniqueName: context.SYNC_LIST_NAME
-        })
-        .then(sync_list => sync_list)
-        .catch((err) => { throw new Error(err.details) });
+      .syncLists
+      .create({
+        uniqueName: 'MagicTexters'
+      })
+      .then(sync_list => sync_list)
+      .catch((err) => {
+        throw new Error(err.details)
+      });
   }
 
+  // Get the SID of the number previously specified in the ENV
   function getPhoneNumberSid() {
     return new Promise((resolve, reject) => {
       client.incomingPhoneNumbers
-        .list({ phoneNumber: context.TWILIO_PHONE_NUMBER, limit: 1 })
+        .list({
+          phoneNumber: context.TWILIO_PHONE_NUMBER,
+          limit: 1
+        })
         .then((incomingPhoneNumbers) => {
           const n = incomingPhoneNumbers[0];
           resolve(n.sid);
@@ -65,6 +82,7 @@ exports.handler = async function (context, event, callback) {
     });
   }
 
+  // Using the number's SID, update it's webhook to the Studio Flow
   function updatePhoneNumberWebhook(webhook, numberSid) {
     return new Promise((resolve, reject) => {
       client.incomingPhoneNumbers(numberSid)
@@ -78,13 +96,18 @@ exports.handler = async function (context, event, callback) {
     });
   }
 
+  // Above, we just set up the functions. Now we will actually
+  // call them one-by one to create these entities.
+
   const key = await createApiKey();
   console.log('API Key created: ' + key.sid);
+
   const syncService = await createSyncService();
   console.log('Sync Service created: ' + syncService.sid);
+
   const syncList = await createSyncList(syncService.sid);
   console.log('Sync List created: ' + syncList.sid);
-  
+
   prepStudioFlow();
   const flow = await deployStudio(flowDefinition);
   console.log('Studio delployed: ' + flow.sid);
@@ -94,67 +117,68 @@ exports.handler = async function (context, event, callback) {
   console.log('Phone number sid updated: ' + phoneNumberSid);
 
 
+  // With all of our entities created, we need to store some
+  // of them inside our hosted ENV in order for use later
   const environment = await getCurrentEnvironment();
   await createEnvironmentVariable(environment, 'SYNC_SERVICE_SID', syncService.sid);
   await createEnvironmentVariable(environment, 'TWILIO_API_KEY', key.sid);
   await createEnvironmentVariable(environment, 'TWILIO_API_SECRET', key.secret);
   console.log('Hosted environment variables created');
 
-  return callback(null, {
-    syncServiceSid: syncService.sid
-  });
+  // This will end the function!
+  return callback(null, 'Setup successfully ran!');
 
 
-async function getCurrentEnvironment() {
-  if (context.DOMAIN_NAME && context.DOMAIN_NAME.startsWith("localhost")) {
-    return;
-  }
-  const services = await client.serverless.services.list();
-  for (let service of services) {
-    console.log("Searching for environment. Looping through service: " + service.sid);
-    const environments = await client.serverless
-      .services(service.sid)
-      .environments.list();
-    const environment = environments.find(
-      env => env.domainName === context.DOMAIN_NAME
-    );
-    if (environment) {
-      // Exit the function
-      return environment;
+  /**
+   *
+   * HELPER FUNCTIONS
+   *
+   */
+  
+  // In the steps above we'll need to get the current environment
+  // based on the domain name this function is running in
+  async function getCurrentEnvironment() {
+    if (context.DOMAIN_NAME && context.DOMAIN_NAME.startsWith("localhost")) {
+      return;
+    }
+    const services = await client.serverless.services.list();
+    for (let service of services) {
+      console.log("Searching for environment. Looping through service: " + service.sid);
+      const environments = await client.serverless
+        .services(service.sid)
+        .environments.list();
+      const environment = environments.find(
+        env => env.domainName === context.DOMAIN_NAME
+      );
+      if (environment) {
+        // Exit the function
+        return environment;
+      }
     }
   }
-}
 
-async function getEnvironmentVariables(environment) {
-  return await client.serverless
-    .services(environment.serviceSid)
-    .environments(environment.sid)
-    .variables.list();
-}
+  // With the environment specified, we use the Twilio REST API
+  // to set new envronment variables. Note, this will throw an
+  // error if exists. Hence the duplicate check up top.
+  async function createEnvironmentVariable(environment, key, value) {
+    try {
+      if (!environment) {
+        throw new Error('No Env!')
+      }
+      console.log(`Creating variable ${key}`);
+      await client.serverless
+        .services(environment.serviceSid)
+        .environments(environment.sid)
+        .variables.create({
+          key: key,
+          value: value
+        });
 
-async function getEnvironmentVariable(environment, key) {
-  // The list filter method isn't implemented yet.
-  const envVars = await getEnvironmentVariables(environment);
-  return envVars.find(variable => variable.key === key);
-}
-
-async function createEnvironmentVariable(environment, key, value) {
-  try {
-    if (!environment) {throw new Error('No Env!')}
-    console.log(`Creating variable ${key}`);
-    await client.serverless
-      .services(environment.serviceSid)
-      .environments(environment.sid)
-      .variables.create({
-        key: key,
-        value: value
-      });
-
-  } catch (err) {
-    console.error(`Error creating '${key}' with '${value}': ${err}`);
-    return false;
+    } catch (err) {
+      console.error(`Error creating '${key}' with '${value}': ${err}`);
+      return false;
+    }
+    return true;
   }
-  return true;
-}
 
 };
